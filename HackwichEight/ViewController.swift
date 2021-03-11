d0d40ba78a0278b50b567ca35e68d3371ca2532e
KKDR1998 <77356024+KKDR1998@users.noreply.github.com>
2021-03-11 10:45:07 -1000

    Completed Problem Sets 1 & 2; 3/11/21

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -10,17 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var randomNumberDisplay: UILabel!
+    
     
     @IBOutlet weak var slider: UISlider!
     
-    var currentValue: Int = 0
+    //var currentValue: Int = 0
+    var currentValue = 0
+
+    var targetValue = 0
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        slider.value = 50
+        
+        
+        //making it an Int because it makes it a float, but the current value is an integer; converting the float value, an number that's not an Int, into an Int.
+        currentValue = Int (slider.value)
+        
+        //Int has an instance method called random
+        //targetValue = Int.random(in: 0...100)
+        
+        //cal the function
+        startNewRound()
+        
+        updateTargetLabel()
     
     }
+    
+    
+    func startNewRound () {
+        targetValue = Int.random(in: 0...100)
+        
+        currentValue = Int (slider.value)
+        
+        updateTargetLabel()
+
+        
+    }
+    
+    func updateTargetLabel () {
+        randomNumberDisplay.text = String (targetValue)
+        
+        
+    }
+    
+    
 
     
     
@@ -33,18 +70,23 @@ class ViewController: UIViewController {
     }
     
     
+    // the alert message
     
     @IBAction func myGuessButtonPressed(_ sender: Any) {
     
-    let message = "The value is \(currentValue)"
+        let message = "The value is \(currentValue)" + "\n The Target Value is: \(targetValue)"
     
-    let alert = UIAlertController(title: "Hello World", message: message , preferredStyle: .alert)
+    let alert = UIAlertController(title: "Guess the Number Game", message: message , preferredStyle: .alert)
         
-    let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        //the dismiss button to close the alert message
+    let action = UIAlertAction(title: "New Round", style: .default, handler: nil)
         
     alert.addAction(action)
     
-        present(alert, animated: true, completion: nil)
+    present(alert, animated: true, completion: nil)
+    
+        
+    startNewRound()
     
     
     
